b7dd8faa2a5e55348520426e8bc4b05396bc23f1
Satyam Singh <singh50satyam@gmail.com>
2021-11-16 17:15:17 +0530

    Changes Added

---
+++ src/pages/HomePage/DropDownListComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const DropDownListComponent = () => {
+    return (
+        <div className="bg-primary p-2"><span>ahsdbb</span>
+        </div>
+    )
+}
+
+export default DropDownListComponent
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -9,7 +9,6 @@ import "../../scss/HomePageStyles/homePage.scss";
 import { STARTUPLIST } from "../../shared-data/startuplist";
 import { DATATABLEDATA } from "../../shared-data/dataTable";
 import styled from "styled-components";
-import DataTable from './table'
 
 const PageWrapperContainer = styled.div({
   display: "flex",
@@ -51,7 +50,7 @@ const HomePage = () => {
                     <MapComponent />
                   </div>
                   <div className="col-12 " style={{
-                      flex: "0 0 auto", width: "39%"
+                    flex: "0 0 auto", width: "39%"
                   }}>
                     <ViewChangerComponent />
                   </div>
@@ -69,18 +68,16 @@ const HomePage = () => {
             <div className="row d-flex justify-content-center px-0 mx-0">
               <div className="btn-group text-center col-md-3 border button-togglers">
                 <button
-                  className={`font-500 font-14px font-family-Mont  shadow-none btn btn-radius px-3 ${
-                    startupListActive && "btn-primary text-white"
-                  }`}
+                  className={`font-500 font-14px font-family-Mont  shadow-none btn btn-radius px-3 ${startupListActive && "btn-primary text-white"
+                    }`}
                   onClick={toggleStartUp}
                 >
                   {" "}
                   Startups List
                 </button>
                 <button
-                  className={`font-500 font-14px font-family-Mont  shadow-none btn btn-radius-data-table px-3 ${
-                    !startupListActive && "btn-primary text-white"
-                  }`}
+                  className={`font-500 font-14px font-family-Mont  shadow-none btn btn-radius-data-table px-3 ${!startupListActive && "btn-primary text-white"
+                    }`}
                   onClick={toggleStartUp}
                 >
                   {" "}
@@ -92,7 +89,7 @@ const HomePage = () => {
                   <StartupsListComponent data={startupsListData} />
                 )}
                 {!startupListActive && (
-                  <DataTable  data={dataTableData} />
+                  <DataTableComponent data={dataTableData} />
                 )}
               </div>
             </div>
