85718a1d8bfcb8e2def2147c6b863ec9191c4252
praispranav <praispranav@gmail.com>
2021-12-04 02:59:24 +0530

    Fixed Bug

--- src/pages/HomePage/Map/statesApi.ts
+++ src/pages/HomePage/Map/statesApi.ts
@@ -5,31 +5,31 @@ import { MapType } from "./states";
 import { States } from "./states";
 
 export async function statesDpiit(values: any): Promise<MapType[]> {
-  const findState = (value: string) => {
-    return States.findIndex((state) =>
-      state.accessor.name.toLowerCase().includes(value.toLowerCase())
-    );
-  };
+  // const findState = (value: string) => {
+  //   return States.findIndex((state) =>
+  //     state.accessor.name.toLowerCase().includes(value.toLowerCase())
+  //   );
+  // };
 
-  const statesDataWrapper = (data: any) => {
-    const newArray = new Array();
-    data.forEach((item: any) => {
-      const newObject: any = new Object();
-      const stateIndex = findState(item["stateName"]);
+  // const statesDataWrapper = (data: any) => {
+  //   const newArray = new Array();
+  //   data.forEach((item: any) => {
+  //     const newObject: any = new Object();
+  //     const stateIndex = findState(item["stateName"]);
 
-      newObject["id"] = item["id"];
-      newObject["accessor"] = {
-        id: item["id"],
-        name: item["stateName"],
-      };
-      if (States[stateIndex]) {
-        newObject["d"] = States[stateIndex].d;
-        newArray.push(newObject);
-      }
-    });
-    console.log(newArray);
-    return newArray;
-  };
+  //     newObject["id"] = item["id"];
+  //     newObject["accessor"] = {
+  //       id: item["id"],
+  //       name: item["stateName"],
+  //     };
+  //     if (States[stateIndex]) {
+  //       newObject["d"] = States[stateIndex].d;
+  //       newArray.push(newObject);
+  //     }
+  //   });
+  //   console.log(newArray);
+  //   return newArray;
+  // };
   return new Promise(async (resolve, rejects) => {
     try {
       const response = await axios({
@@ -38,7 +38,7 @@ export async function statesDpiit(values: any): Promise<MapType[]> {
         httpsAgent: new https.Agent({ rejectUnauthorized: false }),
       });
 
-      resolve(statesDataWrapper(response.data));
+      resolve(response.data);
     } catch (error) {
       console.log("Data Fetching Error Dpiit", error);
       rejects([])
